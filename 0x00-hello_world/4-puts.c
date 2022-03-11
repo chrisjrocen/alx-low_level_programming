53685a59438b78a009f5fff5edf96cf7d693d48a
Ocen Chris <43829976+chrisjrocen@users.noreply.github.com>
2022-03-11 03:26:01 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*
-*Description: prints "Programming is like building a multilingual puzzle"
-*
-*Return always 0 (success)
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
